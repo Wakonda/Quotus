0fdec9b295abef39aa495ea00751d91c4770e96e
Wakonda <amatukami@hotmail.fr>
2019-03-22 01:01:41 +0100

    Add image admin

--- src/Repository/ProverbImageRepository.php
+++ src/Repository/ProverbImageRepository.php
@@ -16,4 +16,32 @@ class ProverbImageRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ProverbImage::class);
     }
+
+	public function getDatatablesForIndex($iDisplayStart, $iDisplayLength, $sortByColumn, $sortDirColumn, $sSearch, $count = false)
+	{
+		$qb = $this->createQueryBuilder("ip");
+
+		$aColumns = array( 'pf.text', null, 'pf.id');
+
+		if(!empty($sortDirColumn))
+		   $qb->orderBy($aColumns[$sortByColumn[0]], $sortDirColumn[0]);
+	   
+	    $qb->join('ip.proverb', 'pf');
+		
+		if(!empty($sSearch))
+		{
+			$search = "%".$sSearch."%";
+			$qb->where('pf.text LIKE :search')
+			   ->setParameter('search', $search);
+		}
+		if($count)
+		{
+			$qb->select("COUNT(ip) AS count");
+			return $qb->getQuery()->getSingleScalarResult();
+		}
+		else
+			$qb->setFirstResult($iDisplayStart)->setMaxResults($iDisplayLength);
+
+		return $qb->getQuery()->getResult();
+	}
 }
